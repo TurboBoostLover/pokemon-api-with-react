c17987f8f8a573495e2b45c474e9ae802d377b7a
Nathan Westergard <m800mazdarx8@gmail.com>
2022-08-03 12:15:14 -0600

    Done

---
+++ src/components/PokedexCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import axios from 'axios';
+
+const PokedexCard = ({pokemon, index, setTeam}) => {
+
+    const addToTeam = () => {
+        axios
+              .get(`https://pokeapi.co/api/v2/pokemon/${pokemon.id}`)
+              .then((res) => {
+                setTeam((prevState) => {
+                    if(prevState.length === 6) return prevState
+                    let newState = [...prevState, res.data]
+                    return newState
+                })
+             })
+    }
+
+
+  return (
+    <div className='pokedex-card'>
+      <img className='pokemon-img'
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id
+        }.png`}
+      />
+    <h3>{pokemon.name}</h3>
+      <button onClick={addToTeam}>Add to Team</button>
+    </div>
+  );
+};
+
+export default PokedexCard;
---
+++ src/components/TeamSection.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TeamSection = ({team}) => {
+
+    let display = team.map((pokemon, index) => {
+        return <h3>{pokemon.name}</h3>
+    })
+
+  return (
+    <section className="team-section">
+      <h2>My Team</h2>
+      <div className="team-container">
+        {display}
+      </div>
+    </section>
+  );
+};
+
+export default TeamSection;
